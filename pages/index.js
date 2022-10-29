44566a5a36af27ca32e4a3dad5b1e136633978ee
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-10-29 23:38:58 +0100

    chore: created the profile section

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Footer from "../components/footer";
 import Link, { links } from "../components/link";
 import styles from "../styles/Home.module.css";
+import ingreImg from "../public/images/ingre.svg";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,6 +14,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <section className={styles.main_profile}>
+          <div>
+            <Image src={ingreImg} alt="" />
+          </div>
+          <p id="twitter">Okolo Johnbosco</p>
+          <p id="slack">Okolo Johnbosco</p>
+        </section>
         <div className={styles.main_link}>
           {links.map((link) => (
             <Link id={link.id} key={link.id} href={link.href}>
