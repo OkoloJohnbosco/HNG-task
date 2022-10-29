e9b10f94f10e513572f6026d285401fd3b1901bc
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-10-29 22:29:15 +0100

    chore: added inter google fonts

---
+++ components/link/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "../../styles/link.module.css";
+
+function Link({ id, href, children }) {
+  return (
+    <a
+      id={id}
+      href={href}
+      className={styles.container}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div>
+        <span>{children}</span>
+      </div>
+    </a>
+  );
+}
+
+export default Link;
